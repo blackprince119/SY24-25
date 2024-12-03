4db7e01ce06e10e5c09fc711d11ef84f350f5a71
Prince <cwpri817@learner.dvusd.org>
2024-12-03 13:40:11 -0700

    new project WPF

---
+++ WPFDEX/WPFDEX/MainWindow.xaml.vb
@@ -0,0 +1,6 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Shinobird.Visibility = Visibility.Visible
+        VIsas.Visibility = Visibility.Hidden
+    End Sub
+End Class
